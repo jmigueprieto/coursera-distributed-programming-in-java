c1754efe05ada64ef3f658953084abd26fd75082
Miguel Prieto <hello@mprieto.me>
2020-01-19 23:28:45 -0300

    Mini project 3 solution

--- miniproject_3/src/main/java/edu/coursera/distributed/MatrixMult.java
+++ miniproject_3/src/main/java/edu/coursera/distributed/MatrixMult.java
@@ -51,15 +51,63 @@ public class MatrixMult {
      *                      normal operation.
      */
     public static void parallelMatrixMultiply(Matrix a, Matrix b, Matrix c,
-            final MPI mpi) throws MPIException {
-        for (int i = 0; i < c.getNRows(); i++) {
-            for (int j = 0; j < c.getNCols(); j++) {
-                c.set(i, j, 0.0);
+                                              final MPI mpi) throws MPIException {
+        final int myRank = mpi.MPI_Comm_rank(mpi.MPI_COMM_WORLD);
+        final int size = mpi.MPI_Comm_size(mpi.MPI_COMM_WORLD);
 
+        final int nRows = c.getNRows();
+        final int rowChunk = (nRows + size - 1) / size;
+        final int startRow = myRank * rowChunk;
+        int endRow = (myRank + 1) * rowChunk;
+        if (endRow > nRows) endRow = nRows;
+
+      // System.out.println(String.format("My Rank: %d, Size: %d, Row Chunk: %d, Start: %d, End: %d",
+      //        myRank, size, rowChunk, startRow, endRow));
+
+        mpi.MPI_Bcast(a.getValues(), 0, a.getValues().length, 0, mpi.MPI_COMM_WORLD);
+        mpi.MPI_Bcast(b.getValues(), 0, b.getValues().length, 0, mpi.MPI_COMM_WORLD);
+
+        double aValues[] = a.getValues();
+        double cValues[] = c.getValues();
+        for (int i = startRow; i < endRow; i++) {
+            int aOffset = a.getOffsetOfRow(i);
+            for (int j = 0; j < c.getNCols(); j++) {
+                int cIndex = c.getOffsetOfRow(i) + j;
+                cValues[cIndex] = 0.0;
                 for (int k = 0; k < b.getNRows(); k++) {
-                    c.incr(i, j, a.get(i, k) * b.get(k, j));
+                    c.incr(i, j, aValues[aOffset + k] * b.get(k, j));
                 }
             }
+        }
+
+        if (myRank == 0) {
+            MPI.MPI_Request[] requests = new MPI.MPI_Request[size - 1];
+            for (int i = 1; i < size; i++) {
+                final int rankStartRow = i * rowChunk;
+                int rankEndRow = (i + 1) * rowChunk;
+                if (rankEndRow > nRows) rankEndRow = nRows;
+
+                final int rowOffset = rankStartRow * c.getNCols();
+                final int nElements = (rankEndRow - rankStartRow) * c.getNCols();
+
+                requests[i - 1] = mpi.MPI_Irecv(c.getValues(),
+                        rowOffset,
+                        nElements,
+                        i,
+                        i,
+                        mpi.MPI_COMM_WORLD);
+            }
+            // System.out.println("Rank 0 Waiting");
+            mpi.MPI_Waitall(requests);
+            // System.out.println("Rank 0 DONE");
+        } else {
+            //System.out.println(String.format("Rank: %d sending data", myRank));
+            mpi.MPI_Send(c.getValues(),
+                    startRow * c.getNCols(),
+                    (endRow - startRow) * c.getNCols(),
+                    0,
+                    myRank,
+                    mpi.MPI_COMM_WORLD);
         }
     }
 }
